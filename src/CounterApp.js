dd9a8b091fac5e7b6f21939138223cb703177b11
zrogba <zrogba@gmail.com>
2022-09-08 11:53:12 +0300

    second logic

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -6,13 +6,26 @@ function CounterApp() {
 //let numCounter = 0
 // Declare a new state variable -> "numCounter"
 let [numCounter, setCount ] = useState(0)
+
+let increaseValue = () => {
+    // function to increase the value
+    
+    } 
+    let decreaseValue = () => {
+    // function to decrease the value
+    // condition to ensure the count stays a positive number 
+    }
+    let resetValue = () => {
+    // function to reset the value to 0
+    };
 return (
 <div>
 <h1> {numCounter} </h1>
-<button>+</button>
-<button>-</button>
-<button>Reset</button>
-</div>
+
+<button onClick={increaseValue}>+</button>
+<button onClick={decreaseValue}>-</button>
+<button onClick={resetValue}>Reset</button>
+</div>//Assign onclick values to buttons
 )
 }
 export default CounterApp;
