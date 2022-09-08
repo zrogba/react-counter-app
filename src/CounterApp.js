c9f5ce5b747bdf2f2521014f054b1e258d085169
zrogba <zrogba@gmail.com>
2022-09-08 21:00:52 +0300

    js complete

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,28 +1,34 @@
-import React, { useState } from 'react';
-
-
+import React from 'react';
+import { useState } from 'react';
 
 function CounterApp() {
 //let numCounter = 0
 // Declare a new state variable -> "numCounter"
-let [numCounter, setCount ] = useState(0)
-
-let increaseValue = () => {
-    // function to increase the value
-    
-    } 
-    let decreaseValue = () => {
-    // function to decrease the value
-    // condition to ensure the count stays a positive number 
+    let [numCounter, setCount ] = useState(0);
+ // function to increase the value
+    let increaseValue = () => {
+    setCount((numCounter += 1)); //increases the count by 1
     }
+    let decreaseValue = () => {
+        // function to decrease the value
+ // if condition is met, ensure the count stays a positive number 
+    if (numCounter === 0) {
+        alert("Should stay positive");
+        } else {
+        setCount((numCounter -= 1)); 
+        }
+    };
+    // function to reset the value to 0   
     let resetValue = () => {
-    // function to reset the value to 0
+    setCount(0); //resets the count to 0
+
     };
 return (
-<div>
-<h1> {numCounter} </h1>
+<div className='container'>
+
 
 <button onClick={increaseValue}>+</button>
+<h6> {numCounter} </h6>
 <button onClick={decreaseValue}>-</button>
 <button onClick={resetValue}>Reset</button>
 </div>//Assign onclick values to buttons
